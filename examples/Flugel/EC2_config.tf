e0c31c7993924378d793550916dede8f98c783e9
lunfaaa <48453297+lunfaaa@users.noreply.github.com>
2021-06-21 14:26:37 -0300

    Delete examples/Flugel directory

--- examples/Flugel/EC2_config.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "Flugel" {
-  ami                    = var.aws_ami
-  instance_type          = var.instance_type
-  tags = {
-    Name = "Flugel"
-    Owner = "InfraTeam"
-  }
-}
