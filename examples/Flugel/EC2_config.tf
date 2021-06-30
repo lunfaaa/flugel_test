98c3b87bc2a8023e0857387f1342d4dcbe7b70db
leandro.martinez <leandro.martinez@unxdigital.com>
2021-06-30 15:04:22 -0300

    upload configuration

--- examples/Flugel/EC2_config.tf
+++ examples/Flugel/EC2_config.tf
@@ -1,4 +1,14 @@
-resource "aws_instance" "Flugel" {
+resource "aws_instance" "instance1" {
+  ami                    = var.aws_ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = ["aws_vpc.flugel"]
+  tags = {
+    Name = "Flugel"
+    Owner = "InfraTeam"
+  }
+}
+
+resource "aws_instance" "instance2" {
   ami                    = var.aws_ami
   instance_type          = var.instance_type
   vpc_security_group_ids = ["aws_vpc.flugel"]
--- examples/Flugel/outputs.tf
+++ examples/Flugel/outputs.tf
@@ -1,24 +1,24 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.Flugel.id
+  value       = aws_instance.instance1.id
 }
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.Flugel.public_ip
+  value       = aws_instance.instance1.public_ip
 }
 
 output "instance_public_dns" {
   description = "Public DNS address of the EC2 instance"
-  value       = aws_instance.Flugel.public_dns
+  value       = aws_instance.instance1.public_dns
 }
 
 output "instance_tag" {
   description = "instnace tag"
-  value       = aws_instance.Flugel.tags
+  value       = aws_instance.instance1.tags
 }
 
 #output "s3_tag" {
 #  description = "S3 tag"
-#  value       = aws_s3_bucket.flugel.tags
+#  value       = aws_s3_bucket.instance1.tags
 #}
